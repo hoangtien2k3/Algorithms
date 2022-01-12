f93e5c158485b719fed65322815dfb386c6b91cd
hoangtien2k3 <hoangtien2k3qx1@gmail.com>
2022-01-12 18:36:40 +0700

    8

---
+++ Insertion_Sort.c/1.c
@@ -0,0 +1,44 @@
+
+#include <stdio.h>
+#include <math.h>
+
+/* Hàm sắp xếp sử dụng thuật toán sắp xếp chèn */
+void insertionSort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+        /* Di chuyển các phần tử có giá trị lớn hơn giá trị
+        key về sau một vị trí so với vị trí ban đầu
+        của nó */
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+/* Hàm xuất mảng */
+void printArray(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main()
+{
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+
+    return 0;
+}

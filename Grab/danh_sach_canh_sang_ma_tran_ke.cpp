1b9c69a3bf584c5dc7af68ecac1f5c4c7268d336
Hoàng Tiến <hoangtien2k3qx1@gmail.com>
2022-05-21 23:31:13 +0700

    C++ algorithms

--- Grab/danh_sach_canh_sang_ma_tran_ke.cpp
+++ Grab/danh_sach_canh_sang_ma_tran_ke.cpp
@@ -36,7 +36,7 @@ int main() {
 
     for(int i=1; i<n; i++) {
         for(int j=1; j<n; j++) {
-            cout << arr[i][j] << " ";
+            cout << arr[i][j] << " "; 
         }
         cout << endl;
     }

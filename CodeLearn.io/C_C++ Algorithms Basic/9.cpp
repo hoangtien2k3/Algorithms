7a03aa136915ff5b8204dc3c049f1149ac8e3caf
Hoàng Tiến <hoangtien2k3qx1@gmail.com>
2022-07-02 07:27:19 +0700

    C++ 02/07/2022

--- CodeLearn.io/C_C++ Algorithms Basic/9.cpp
+++ CodeLearn.io/C_C++ Algorithms Basic/9.cpp
@@ -9,44 +9,44 @@
     Đầu ra/Đầu vào:
 
     Đầu vào: integer n
-    1 <= n <= 10^12
+        1 <= n <= 10^12
     Output:
     Số chữ số 0 tận cùng của n!
     Thời gian chạy: 0.5 giây
 
+
+! giải thích:
+    Tại sao n>=5 bời vì bắt đầu từ 5! ta mới có
+    2 số là 2 và 5 nên mới có số 0 tận cùng
+    Ý tưởng là : ta lấy n chia cho 5 cho đến khi mà n không chia
+    được cho 5 nữa (không quan tâm chia hết hay không chia hết)
+    thì tổng kết quả của những lần chia đó sẽ là những số 0 đằng sau
+    Vd ta muốn tính 100! thì ta lấy 100/5=20/5=4<5
+    Số Những 0 đằng sau sẽ là 20+4;
+    VD 5000! thì 5000/5=1000/5=200/5=40/5=8/5=1
+    1000+200+40+8+1=1249
 */
-#include<bits/stdc++.h>
 
-// int solution(int n) {
-//     int d = 0;
-//     int k = 5;
-//     while(k <= n) {
-//         d += (n / k);
-//         k *= 5;
-//     }
-//     return d;
-// }
 
-int giaithua(int n) {
-    if (n == 0 || n == 1) return 1;
-    return n * giaithua(n-1);
-}
+#include<iostream>
+#include<cmath>
+using namespace std;
 
-int solution(int n) {
-    int count = 0;
-    int ans = giaithua(n);
-    while(n != 0) {
-        if (ans % 10 == 0) {
-            count++;
-            n /= 10;
-        }
+long long numberZeroDigits(long long n) {
+    long long count = 0;
+    while(n >= 5) {
+        count += n/5;
+        n /= 5;
     }
     return count;
 }
 
 int main() {
-    int n;
-    std::cin >> n;
-    std::cout << solution(n);
+    long long n;
+    cin >> n;
+    cout << numberZeroDigits(n);
     return 0;
 }
+
+
+
